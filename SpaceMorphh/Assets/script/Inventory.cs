30ac24fbadf03e048989780dc15b475d3f12a5a5
jorge-puentes-b <jorgepb2007@gmail.com>
2024-10-15 01:17:11 -0500

    Add Scenes jorge and Create Script Slot,,Item and Inventory Update
    Add Scenes jorge and Create Script Slot,,Item and Inventory Update

--- SpaceMorphh/Assets/script/Inventory.cs
+++ SpaceMorphh/Assets/script/Inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
@@ -20,6 +21,12 @@ public class Inventory : MonoBehaviour
         for (int i = 0; i < allSlots; i++)
         {
             slot[i] = slotHolder.transform.GetChild(i).gameObject;
+
+
+            if (slot[i].GetComponent<Slot>().item == null)
+            {
+                slot[i].GetComponent<Slot>().empy = true;
+            }
         }
     }
 
@@ -38,6 +45,43 @@ public class Inventory : MonoBehaviour
         else
         {
             inventory.SetActive(false); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item") 
+        {
+            GameObject itemPicketUp = other.gameObject;
+
+            Item item = itemPicketUp.GetComponent<Item>();
+
+            AddItem(itemPicketUp, item.ID, item.type, item.descripcion,item.icon);
+        }
+    }
+
+    public void AddItem(GameObject itemObject, int itemID, string itemType , string itemDescripcion, Sprite itemIcon) 
+    {
+        for (int i = 0; i < allSlots; i++) 
+        {
+            if (slot[i].GetComponent<Slot>().empy)
+            {
+                itemObject.GetComponent<Item>().pickedUp = true;
+
+                slot[i].GetComponent<Slot>().item = itemObject;
+                slot[i].GetComponent<Slot>().ID = itemID;
+                slot[i].GetComponent<Slot>().type = itemType;
+                slot[i].GetComponent<Slot>().description = itemDescripcion;
+                slot[i].GetComponent<Slot>().icon = itemIcon;
+
+                itemObject.transform.parent = slot[i].transform;
+                itemObject.SetActive(false);
+
+                slot[i].GetComponent<Slot>().UpdateSlot();
+
+                slot[i].GetComponent<Slot>().empy = false;
+            }
+            return;
         }
     }
 }
